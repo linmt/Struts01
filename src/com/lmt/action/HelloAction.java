4a91fc2bc00b23926e7aeca5c870c7fc3f742ae7
linmt <549343315@qq.com>
2019-01-01 22:23:06 +0800

    190101    hello案例测试成功

---
+++ src/com/lmt/action/HelloAction.java
@@ -0,0 +1,11 @@
+package com.lmt.action;
+
+/**
+ * Created by 热带雨林 on 2019/1/1.
+ */
+public class HelloAction {
+    public String sayHello() {
+        System.out.println("Hello,Action.");
+        return "ok";
+    }
+}
